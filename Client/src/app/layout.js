c9ef8576cddf06724045aada0b15ee77835523f6
vuducanh221104 <vuducanh22112004@gmail.com>
2024-09-15 22:58:08 +0700

    fix: backend

--- Client/src/app/layout.js
+++ Client/src/app/layout.js
@@ -18,6 +18,34 @@ export const metadata = {
   icons: {
     icon: "/favicon.ico",
   },
+  openGraph: {
+    title: "Dinhhavn",
+    description: "dinhhavn.id.vn",
+    url: "https://dinhhavn.id.vn",
+    type: "website",
+    images: [
+      {
+        url: "https://res.cloudinary.com/do4zld720/image/upload/v1726413958/favicon_iutkcy.png",
+        alt: "Dinhhavn.id.vn",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@dinhhavn",
+    title: "Dinhhavn",
+    description: "dinhhavn.id.vn",
+    images: [
+      {
+        url: "https://res.cloudinary.com/do4zld720/image/upload/v1726413958/favicon_iutkcy.png",
+        alt: "Dinhhavn.id.vn",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
